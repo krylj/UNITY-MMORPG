e7e1e6e9aee15e6749e703c87546730425ec1de4
JoZi <107387653+LostJoZi@users.noreply.github.com>
2025-01-26 10:05:42 +0100

    puopuv bvordel nedoporucuju stahovat

---
+++ scriptpalcat.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptpalcat : MonoBehaviour
+{
+    public Animator ani;
+    public AudioSource ass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            ani.Play("palcatani");
+            ass.Play();
+        }
+    }
+}
