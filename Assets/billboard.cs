ec87acc86b2df3e6cbfdfdfabefeb49663473e86
Komrskus <lukas.komrska@gmail.com>
2025-02-23 14:12:33 +0100

    1

---
+++ Assets/billboard.cs
@@ -0,0 +1,16 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : NetworkBehaviour
+{
+
+    // Standard Billboard script which makes canvas objects always look
+    // at the camera
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + DynamicTextManager.mainCamera.forward);
+    }
+}
