284c7ae6dbae621c160686da6ce4b1be46af60d1
krylj <krystofhorna2007@gmail.com>
2025-03-30 11:52:51 +0200

    slime

---
+++ Assets/mobslimet1ai.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections;
+
+public class Mob : MonoBehaviour
+{
+    public string mobName = "DefaultMob"; public int maxHP = 100; public int currentHP; public int attackPower = 10; public float attackRange = 2.0f; public float detectionRange = 10.0f; public float moveSpeed = 3.0f;
+
+    private Transform player;
+    private bool isChasing = false;
+    private bool isAttacking = false;
+
+    void Start()
+    {
+        currentHP = maxHP;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= detectionRange && !isAttacking)
+        {
+            isChasing = true;
+        }
+        else if (distanceToPlayer > detectionRange)
+        {
+            isChasing = false;
+        }
+
+        if (isChasing)
+        {
+            ChasePlayer();
+        }
+    }
+
+    void ChasePlayer()
+    {
+        if (player != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+            transform.LookAt(player);
+
+            if (Vector3.Distance(transform.position, player.position) <= attackRange)
+            {
+                StartCoroutine(AttackPlayer());
+            }
+        }
+    }
+
+    IEnumerator AttackPlayer()
+    {
+        isAttacking = true;
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log(mobName + " attacks the player for " + attackPower + " damage!");
+        isAttacking = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        Debug.Log(mobName + " takes " + damage + " damage!");
+
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log(mobName + " has died!");
+        Destroy(gameObject);
+    }
+
+}
